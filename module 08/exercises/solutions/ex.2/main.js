96627c54fb7e1ed596597bacaeb487e62de7d219
Kobi Hari <kobi2294@yahoo.com>
2024-02-20 16:38:59 +0200

    ex 2 back

--- module 08/exercises/solutions/ex.2/main.js
+++ module 08/exercises/solutions/ex.2/main.js
@@ -1,5 +1,3 @@
-var colors = ['red', 'green', 'blue', 'yellow', 'brown', 'purple'];
-
 function changeHeaderToColor(color) {
     var e = document.getElementById('my-header');
     e.className = color;
